b5405d0335d1edb8bbfa4ec007c255742a772e23
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-09-22 22:55:13 -0400

    add files for class for AR processing on separate thread from video capture

---
+++ Controller/Processing/ProcessingThread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Raphael Russo on 9/22/24.
+//
+
+#include "ProcessingThread.h"
---
+++ Controller/Processing/ProcessingThread.h
@@ -0,0 +1,14 @@
+//
+// Created by Raphael Russo on 9/22/24.
+//
+
+#ifndef AR_SETTLERS_PROCESSINGTHREAD_H
+#define AR_SETTLERS_PROCESSINGTHREAD_H
+
+
+class ProcessingThread {
+
+};
+
+
+#endif //AR_SETTLERS_PROCESSINGTHREAD_H
