0222bd059a52fa7c35fbfb415b0c22bb0f22f94d
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-09-24 16:29:19 -0400

    update gui to use command pattern

---
+++ Controller/Commands/IncrementScoreCommand.h
@@ -0,0 +1,33 @@
+//
+// Created by Raphael Russo on 9/24/24.
+//
+
+#ifndef AR_SETTLERS_INCREMENTSCORECOMMAND_H
+#define AR_SETTLERS_INCREMENTSCORECOMMAND_H
+#pragma once
+#include "Command.h"
+#include "../../Model/GameModel.h"
+#include "../PlayerInfoBuilder.h"
+
+class IncrementScoreCommand : public Command {
+private:
+    GameModel* model;
+    QString color;
+
+public:
+    IncrementScoreCommand(GameModel* model, const QString& color)
+            : model(model), color(color) {}
+
+    QMap<QString, QVariant> run() override {
+        Player* player = model->getPlayer(color.toStdString());
+        if (player) {
+            player->incrementScore();
+            return PlayerInfoBuilder()
+                    .setColor(color)
+                    .setScore(player->getScore())
+                    .build();
+        }
+        return {};
+    }
+};
+#endif //AR_SETTLERS_INCREMENTSCORECOMMAND_H
--- View/GUIView.cpp
+++ View/GUIView.cpp
@@ -10,17 +10,21 @@
 GUIView::GUIView(Controller* controller, QWidget* parent)
         : QWidget(parent), controller(controller) {
     QVBoxLayout* mainLayout = new QVBoxLayout;
+
+    // Video
     videoLabel = new QLabel(this);
     videoLabel->setFixedSize(640, 480);
     mainLayout->addWidget(videoLabel);
 
-    // Switch video source buttons
+    // Switch video source buttons and layout
+    QHBoxLayout* videoButtonsLayout = new QHBoxLayout;
     QPushButton* cameraButton = new QPushButton("Switch to Camera", this);
     QPushButton* rtspButton = new QPushButton("Switch to RTSP Stream", this);
-    mainLayout->addWidget(cameraButton);
-    mainLayout->addWidget(rtspButton);
+    videoButtonsLayout->addWidget(cameraButton);
+    videoButtonsLayout->addWidget(rtspButton);
+    mainLayout->addLayout(videoButtonsLayout);
 
-    // Connect buttons to controller slots
+    // Connect buttons to controller slots for video
     connect(cameraButton, &QPushButton::clicked, controller, &Controller::switchToCamera);
     connect(rtspButton, &QPushButton::clicked, [this]() {
         QString rtspUrl = QInputDialog::getText(this, "Enter RTSP URL", "RTSP URL:");
@@ -29,8 +33,38 @@ GUIView::GUIView(Controller* controller, QWidget* parent)
         }
     });
 
-    // Connect controllers frameReady signal to updateVideoFeed slot
+    // Rolling dice
+    QHBoxLayout* diceLayout = new QHBoxLayout;
+    rollDiceButton = new QPushButton("Roll Dice", this);
+    diceRollLabel = new QLabel("Dice Roll: ", this);
+    diceLayout->addWidget(rollDiceButton);
+    diceLayout->addWidget(diceRollLabel);
+    mainLayout->addLayout(diceLayout);
+    connect(rollDiceButton, &QPushButton::clicked, controller, &Controller::rollDice);
+
+    // Finish game button
+    finishGameButton = new QPushButton("Finish Game", this);
+    mainLayout->addWidget(finishGameButton);
+
+    connect(finishGameButton, &QPushButton::clicked, controller, &Controller::finishGame);
+
+    // Player panels
+    QGridLayout* playersLayout = new QGridLayout;
+
+    QStringList colors = {"blue", "red", "orange", "white"};
+    int col = 0;
+    for (const QString& color : colors) {
+        QGroupBox* playerPanel = createPlayerPanel(color);
+        playersLayout->addWidget(playerPanel, 0, col++);
+        playerPanels[color] = playerPanel;
+    }
+    mainLayout->addLayout(playersLayout);
+
+
+    // Connect controllers signals to slots here in view
     connect(controller, &Controller::frameReady, this, &GUIView::updateVideoFeed);
+    connect(controller, &Controller::diceRolled, this, &GUIView::updateDiceRoll);
+    connect(controller, &Controller::playerUpdated, this, &GUIView::updatePlayerInfo);
 
     setLayout(mainLayout);
 }
@@ -46,4 +80,106 @@ void GUIView::updateVideoFeed(const cv::Mat& frame) {
         QImage qImg(rgbFrame.data, rgbFrame.cols, rgbFrame.rows, rgbFrame.step, QImage::Format_RGB888);
         videoLabel->setPixmap(QPixmap::fromImage(qImg).scaled(videoLabel->size(), Qt::KeepAspectRatio));
     }
+}
+
+void GUIView::updateDiceRoll(int roll) {
+    diceRollLabel->setText(QString("Dice Roll: %1").arg(roll));
+}
+
+/*
+
+void GUIView::updatePlayerInfo(const QString& color, const Player& player) {
+    QGroupBox* panel = playerPanels[color];
+    if (panel) {
+        QLineEdit* usernameEdit = panel->findChild<QLineEdit*>("usernameEdit");
+        QLabel* scoreLabel = panel->findChild<QLabel*>("scoreLabel");
+        QLabel* statsLabel = panel->findChild<QLabel*>("statsLabel");
+
+        if (usernameEdit) {
+            usernameEdit->setText(QString::fromStdString(player.getUsername()));
+        }
+        if (scoreLabel) {
+            scoreLabel->setText(QString("Score: %1").arg(player.getScore()));
+        }
+        if (statsLabel) {
+            statsLabel->setText(QString("Wins: %1\nGames Played: %2\nAvg Score: %3")
+                                        .arg(player.getWins())
+                                        .arg(player.getGamesPlayed())
+                                        .arg(player.getAverageScore()));
+        }
+    }
+}
+ */
+
+void GUIView::updatePlayerInfo(const QMap<QString, QVariant>& playerInfo) {
+    QString color = playerInfo["color"].toString();
+    QGroupBox* panel = playerPanels[color];
+    if (panel) {
+        QLineEdit* usernameEdit = panel->findChild<QLineEdit*>("usernameEdit");
+        QLabel* scoreLabel = panel->findChild<QLabel*>("scoreLabel");
+        QLabel* statsLabel = panel->findChild<QLabel*>("statsLabel");
+
+        if (usernameEdit && playerInfo.contains("username")) {
+            usernameEdit->setText(playerInfo["username"].toString());
+        }
+        if (scoreLabel && playerInfo.contains("score")) {
+            scoreLabel->setText(QString("Score: %1").arg(playerInfo["score"].toInt()));
+        }
+        if (statsLabel) {
+            QString stats = "Wins: %1\nGames Played: %2\nAvg Score: %3";
+            stats = stats.arg(playerInfo.value("wins", 0).toInt())
+                    .arg(playerInfo.value("gamesPlayed", 0).toInt())
+                    .arg(playerInfo.value("averageScore", 0.0).toDouble(), 0, 'f', 2);
+            statsLabel->setText(stats);
+        }
+    }
+}
+
+QGroupBox* GUIView::createPlayerPanel(const QString& color) {
+
+    QGroupBox* groupBox = new QGroupBox(color.toUpper() + " PLAYER", this);
+    //color.toLower();
+    QVBoxLayout* vbox = new QVBoxLayout;
+
+    // Username
+    QLineEdit* usernameEdit = new QLineEdit("guest", this);
+    usernameEdit->setObjectName("usernameEdit");
+    usernameEdit->setFocusPolicy(Qt::ClickFocus);
+    vbox->addWidget(new QLabel("Username:", this));
+    vbox->addWidget(usernameEdit);
+
+
+    // Connect username edit to controller
+    connect(usernameEdit, &QLineEdit::editingFinished, [this, color, usernameEdit]() {
+        QString username = usernameEdit->text();
+        this->controller->updatePlayerUsername(color, username);
+        usernameEdit->clearFocus();
+    });
+
+    // Score with increment and decrement buttons
+    QHBoxLayout* scoreLayout = new QHBoxLayout;
+    QPushButton* decrementButton = new QPushButton("-", this);
+    QLabel* scoreLabel = new QLabel("Score: 0", this);
+    scoreLabel->setObjectName("scoreLabel");
+    QPushButton* incrementButton = new QPushButton("+", this);
+    scoreLayout->addWidget(decrementButton);
+    scoreLayout->addWidget(scoreLabel);
+    scoreLayout->addWidget(incrementButton);
+    vbox->addLayout(scoreLayout);
+
+    // Connect score buttons to controller
+    connect(incrementButton, &QPushButton::clicked, [this, color]() {
+        this->controller->incrementPlayerScore(color);
+    });
+    connect(decrementButton, &QPushButton::clicked, [this, color]() {
+        this->controller->decrementPlayerScore(color);
+    });
+
+    // Player stats
+    QLabel* statsLabel = new QLabel("Wins: 0\nGames Played: 0\nAvg Score: 0.0", this);
+    statsLabel->setObjectName("statsLabel");
+    vbox->addWidget(statsLabel);
+
+    groupBox->setLayout(vbox);
+    return groupBox;
 }
