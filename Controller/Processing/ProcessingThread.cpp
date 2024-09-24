6718a19016f5b430b94451a6c1dfcfa38df6369e
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-09-24 10:19:30 -0400

    add new thread for processing markers

--- Controller/Processing/ProcessingThread.cpp
+++ Controller/Processing/ProcessingThread.cpp
@@ -3,3 +3,70 @@
 //
 
 #include "ProcessingThread.h"
+
+#include <opencv2/aruco.hpp>
+
+ProcessingThread::ProcessingThread(GameModel *model, QObject *parent) : QThread(parent), model(model), stopThread(false),
+        dictionary(cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_1000)), detectorParams(cv::aruco::DetectorParameters()),
+        detector(dictionary, detectorParams){}
+
+ProcessingThread::~ProcessingThread() {
+    mutex.lock();
+    stopThread = true;
+    frameAvailable.wakeOne();
+    mutex.unlock();
+    wait();
+}
+
+void ProcessingThread::enqueueFrame(const cv::Mat &frame) {
+    QMutexLocker locker(&mutex);
+    frameQueue.enqueue(frame.clone());
+    frameAvailable.wakeOne();
+}
+
+void ProcessingThread::run() {
+    while (true) {
+        cv::Mat frame;
+        mutex.lock();
+        if (frameQueue.isEmpty()) {
+            frameAvailable.wait(&mutex);
+        }
+        if (stopThread) {
+            mutex.unlock();
+            break;
+        }
+        if (!frameQueue.isEmpty()) {
+            frame = frameQueue.dequeue();
+        }
+        mutex.unlock();
+        if (!frame.empty()) {
+            processFrame(frame);
+        }
+    }
+}
+
+void ProcessingThread::processFrame(const cv::Mat &frame) {
+
+    // Detect markers
+    cv::Mat processedFrame = frame.clone();
+    std::vector<int> markerIDs;
+    std::vector<std::vector<cv::Point2f>> markerCorners;
+    std::vector<std::vector<cv::Point2f>> rejectedCandidates;
+
+    detector.detectMarkers(processedFrame, markerCorners, markerIDs, rejectedCandidates);
+
+    // Render objects based on the markers detected
+    if (!markerIDs.empty()) {
+        for (size_t i = 0; i < markerIDs.size(); ++i) {
+            int markerID = markerIDs[i];
+            ARObject* arObject = model->getObject(markerID);
+            if (arObject) {
+                arObject->render(processedFrame);
+            }
+        }
+    }
+
+    // Emit processed frame
+    emit frameProcessed(processedFrame);
+
+}
--- Controller/Processing/ProcessingThread.h
+++ Controller/Processing/ProcessingThread.h
@@ -4,9 +4,45 @@
 
 #ifndef AR_SETTLERS_PROCESSINGTHREAD_H
 #define AR_SETTLERS_PROCESSINGTHREAD_H
+#pragma once
+
+#include <QThread>
+#include <opencv2/opencv.hpp>
+#include <QMutex>
+#include <QQueue>
+#include <QWaitCondition>
+#include "../../Model/GameModel.h"
+
+class ProcessingThread : public QThread {
+Q_OBJECT
+public:
+    ProcessingThread(GameModel *model, QObject *parent = nullptr);
+    ~ProcessingThread();
+
+    void enqueueFrame(const cv::Mat& frame);
+
+signals:
+    void frameProcessed(const cv::Mat& frame);
+
+protected:
+    void run() override;
+
+private:
+    GameModel *model;
+    QMutex mutex;
+    QQueue<cv::Mat> frameQueue;
+    QWaitCondition frameAvailable;
+
+    bool stopThread;
+
+    // Detection stuff
+    cv::aruco::Dictionary dictionary; // Dictionary of the markers
+    cv::aruco::DetectorParameters detectorParams;
+    cv::aruco::ArucoDetector detector;
+
+    void processFrame(const cv::Mat &frame);
 
 
-class ProcessingThread {
 
 };
 
