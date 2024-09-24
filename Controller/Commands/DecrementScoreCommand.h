8b968fb2cf26340b837e2176782525072ad09c2d
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-09-24 16:30:42 -0400

    add command pattern to controller and builder pattern for player info

---
+++ Controller/Commands/DecrementScoreCommand.h
@@ -0,0 +1,33 @@
+//
+// Created by Raphael Russo on 9/24/24.
+//
+
+#ifndef AR_SETTLERS_DECREMENTSCORECOMMAND_H
+#define AR_SETTLERS_DECREMENTSCORECOMMAND_H
+#pragma once
+#include "Command.h"
+#include "../../Model/GameModel.h"
+#include "../PlayerInfoBuilder.h"
+
+class DecrementScoreCommand : public Command {
+private:
+    GameModel* model;
+    QString color;
+
+public:
+    DecrementScoreCommand(GameModel* model, const QString& color)
+            : model(model), color(color) {}
+
+    QMap<QString, QVariant> run() override {
+        Player* player = model->getPlayer(color.toStdString());
+        if (player) {
+            player->decrementScore();
+            return PlayerInfoBuilder()
+                    .setColor(color)
+                    .setScore(player->getScore())
+                    .build();
+        }
+        return {};
+    }
+};
+#endif //AR_SETTLERS_DECREMENTSCORECOMMAND_H
--- Controller/Controller.h
+++ Controller/Controller.h
@@ -8,11 +8,23 @@
 
 #include <QObject>
 #include <opencv2/opencv.hpp>
+#include <QtCore/QMap>
 #include "Video/VideoInput.h"
 #include "Video/VideoCaptureThread.h"
 #include "../Model/GameModel.h"
 #include "Video/RTSPInput.h"
 #include "Video/CameraInput.h"
+#include "Processing/ProcessingThread.h"
+#include "Commands/Command.h"
+
+struct PlayerInfo {
+    QString color;
+    QString username;
+    int score;
+    int wins;
+    int gamesPlayed;
+    double averageScore;
+};
 
 
 class Controller : public QObject {
@@ -30,19 +42,46 @@ public slots:
     void switchToCamera();
     void switchToRTSP(const QString& rtspUrl);
 
+    // Slots for view interaction
+    void rollDice();
+    void finishGame();
+    void updatePlayerUsername(const QString& color, const QString& username);
+    void incrementPlayerScore(const QString& color);
+    void decrementPlayerScore(const QString& color);
+    void onFrameProcessed(const cv::Mat& frame);
+
 signals:
     // Signal to send processed frame to View
     void frameReady(const cv::Mat& frame);
+
+
+
+    // Signal for dice roll
+    void diceRolled(int roll);
+
+    // Signal for player update
+    //void playerUpdated(const QString& color, const Player& player);
+    void playerUpdated(const QMap<QString, QVariant>& playerInfo);
+
 
 private:
     GameModel *model;
     VideoInput *currentInput;  // Ptr for curr source
     VideoCaptureThread *captureThread;
+    ProcessingThread *processingThread;
 
     CameraInput *cameraInput;  // Built in cam
     RTSPInput *rtspInput;      // RPi input --- COME BACK TO THIS LATER WHEN I HAVE THE RASPBERRY PI ON ME
 
     void stopCurrentInput();
+
+    void runCommand(std::unique_ptr<Command> command) {
+        QMap<QString, QVariant> result = command->run();
+        if (!result.isEmpty()) {
+            emit playerUpdated(result);
+        }
+    }
+
 
     // Method to detect markers and update the model
     // void markerdetect/modelupdate(cv::Mat &frame);
