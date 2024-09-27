d524c147b5df719c395c58af99a9ae7a62c24290
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-09-26 22:21:00 -0400

    add camera matrix and distortion coeffs to config

--- Config.h
+++ Config.h
@@ -27,6 +27,16 @@ public:
         return markerLength;
     }
 
+    cv::Mat getCameraMatrix() const {
+        std::lock_guard<std::mutex> lock(configMutex);
+        return cameraMatrix;
+    }
+
+    std::vector<double> getDistCoeffs() const {
+        std::lock_guard<std::mutex> lock(configMutex);
+        return distCoeffs;
+    }
+
 
     void setMarkerLength(float length) {
         std::lock_guard<std::mutex> lock(configMutex);
@@ -38,10 +48,25 @@ public:
 
 private:
 
-    Config() : markerLength(0.038f) {}
+    Config() : markerLength(0.038f) {
+        try {
+            cv::FileStorage fs("camera_calibration_setting.xml", cv::FileStorage::READ);
+            if (fs.isOpened()) {
+                fs["camera_matrix"] >> cameraMatrix;
+                fs["distortion_coefficients"] >> distCoeffs;
+                fs.release();
+            }
+        } catch (int e) {
+            printf("Error with loading calibration");
+        }
+    }
 
 
     float markerLength;
+    cv::Mat cameraMatrix;
+    std::vector<double> distCoeffs;
+
+
 
 
     mutable std::mutex configMutex;
